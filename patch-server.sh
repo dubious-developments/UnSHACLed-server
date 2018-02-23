81d5bef039d6f321b43436686163e84822f57a77
jonathanvdc <jonathan.vdc@outlook.com>
2018-02-23 16:13:43 +0100

    Tweak patch-server.sh

--- patch-server.sh
+++ patch-server.sh
@@ -4,5 +4,4 @@ current_dir_path=$(dirname $0)
 
 sed 's:UNSHACLED_PATH:'$1':g' $current_dir_path/sites-enabled-default \
     > $current_dir_path/sites-enabled-default.patched
-sed 's:TCP_PORT:'$2':g' $current_dir_path/sites-enabled-default.patched \
-    > $current_dir_path/sites-enabled-default.patched
+sed -i 's:TCP_PORT:'$2':g' $current_dir_path/sites-enabled-default.patched
